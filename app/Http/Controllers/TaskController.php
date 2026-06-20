4343f4a0188642491e65a82f0a5c373f04447ea9
Uhti Amelia <uhtiamelia06@gmail.com>
2026-06-20 09:22:05 +0700

    testing vercel

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,14 +7,10 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-
-   public function index()
-{
-    $tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-}
-
+    public function index()
+    {
+        return 'Laravel berhasil berjalan';
+    }
 
     public function create()
     {
